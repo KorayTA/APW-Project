7b512fb3b7b6f97e90f4d8c42182e48cc4d14586
Richard Shinnick <rshinnick4@gmail.com>
2021-04-29 17:17:29 -0400

    Update

--- HTML/app.js
+++ HTML/app.js
@@ -10,21 +10,41 @@ app.set('view engine', 'pug');
 
 //Home Route
 app.get('/', function(req,res) {
-res.render('Website');
+res.render('Website', {
+    title: 'APW Dealership',
+    title1: 'Find your dream-car with us!'
+});
 });
 
 //Add routes
 app.get('/CreateAccount', function(req, res) {
-    res.render('createAccount')
-})
+    res.render('createAccount', {
+        title: 'APW Dealership',
+        title1: 'Find your dream-car with us!',
+        title3: 'Create-Account:',
+        title4: 'Please fill out the boxes below with your information.'
+    });
+});
 
 app.get('/Sign-In', function(req, res) {
-    res.render('Sign-in')
-})
+    res.render('Sign-in', {
+        title: 'APW Dealership',
+        title1: 'Find your dream-car with us!',
+        title3: 'Sign-in:',
+        title4: 'Please provide us with your email and password. Then tell us if you are a staff member or not.'
+    });
+});
 
 app.get('/Create-A-Car', function(req, res) {
-    res.render('Create-A-Car')
-})
+    res.render('Create-A-Car', {
+        title: 'APW Dealership',
+        title1: 'Find your dream-car with us!',
+        title3: 'Car Search:',
+        title4: 'Below you will find a series of options that will help us find the perfect car for you.',
+        title5: 'If there is an option that you are unsure of, leave it blank! We will provide examples',
+        title6: 'based on the other information that you have given us.'
+    });
+});
 
 //Start server
 app.listen(3000, function() {
